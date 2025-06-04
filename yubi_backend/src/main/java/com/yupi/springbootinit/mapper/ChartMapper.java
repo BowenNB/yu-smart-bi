785ee05d1cd5c912925bd9f1d86bff9c7ba9acf0
BowenNB <2923591580@qq.com>
2025-06-04 13:09:11 +0800

    首次提交

---
+++ yubi_backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,17 @@
+package com.yupi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.Chart;
+/**
+* @author Lenovo
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2025-04-24 10:30:53
+* @Entity generator.domain.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ yubi_backend/src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User ;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Lenovo
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-04-24 10:30:53
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+

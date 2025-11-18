1ee8229d28b9ece7ce8ed930e271aeee81942d96
BowenNB <2923591580@qq.com>
2025-11-18 16:46:44 +0800

    20251118

--- yubi_frontend/src/app.tsx
+++ yubi_frontend/src/app.tsx
@@ -53,7 +53,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
       },
     },
     waterMarkProps: {
-      content: initialState?.currentUser?.name,
+      content: initialState?.currentUser?.userName,
     },
     footerRender: () => <Footer />,
     onPageChange: () => {

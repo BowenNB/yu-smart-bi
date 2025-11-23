aaae0f79360047e1fd401ba7e48ed14d3f844c19
Bowen <18105622016@163.com>
2025-11-23 21:17:07 +0800

    feat(api): 增加Excel文件处理和读取功能

--- yubi_backend/src/main/java/com/yupi/springbootinit/utils/ExcelUtils.java
+++ yubi_backend/src/main/java/com/yupi/springbootinit/utils/ExcelUtils.java
@@ -6,6 +6,7 @@ import com.alibaba.excel.read.builder.ExcelReaderBuilder;
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.qcloud.cos.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,40 +24,46 @@ import java.util.stream.Collectors;
 /**
  * Excel 工具类
  */
+@Slf4j
 public class ExcelUtils {
 
     public static String excelToCsv(MultipartFile multipartFile) throws FileNotFoundException {
-        File file = null;
-        try{
-            file = ResourceUtils.getFile("classpath:网站数据.xlsx");
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
+//        File file = null;
+//        try{
+//            file = ResourceUtils.getFile("classpath:网站数据.xlsx");
+//        }catch (FileNotFoundException e){
+//            e.printStackTrace();
+//        }
 
         //读取数据
-         List<Map<Integer, String>> list = EasyExcel.read(file)
-                 .excelType(ExcelTypeEnum.XLSX)
-                 .sheet()
-                 .headRowNumber(0)
-                 .doReadSync();
+        List<Map<Integer, String>> list = null;
+        try {
+            list = EasyExcel.read(multipartFile.getInputStream())
+                    .excelType(ExcelTypeEnum.XLSX)
+                    .sheet()
+                    .headRowNumber(0)
+                    .doReadSync();
+        } catch (IOException e) {
+            log.error("读取Excel失败", e);// 这里的e是什么意思？
+        }
         //如果数据为空
         if(CollectionUtils.isEmpty(list)){
             return "";
         }
         //转换为csv
+        StringBuilder stringbuilder = new StringBuilder();
         //读取表头（第一行）
         LinkedHashMap<Integer, String> headerMap = (LinkedHashMap<Integer, String>) list.get(0);
         List<String> headerList = headerMap.values().stream().filter(ObjectUtils::isNotEmpty)
                 .collect(Collectors.toList());
-        System.out.println(StringUtils.join(String.valueOf(headerList),","));
+        stringbuilder.append(StringUtils.join(String.valueOf(headerList),",")).append("\n");
         //读取数据
         for(int i=1;i<list.size();i++){
             LinkedHashMap<Integer, String> dataMap = (LinkedHashMap<Integer, String>) list.get(i);
             List<String> datalist = dataMap.values().stream().filter(ObjectUtils::isNotEmpty).collect(Collectors.toList());
-            System.out.println(StringUtils.join(String.valueOf(datalist),","));
+            stringbuilder.append(StringUtils.join(String.valueOf(datalist),",")).append("\n");
         }
-        System.out.println(list);
-        return "";
+        return stringbuilder.toString();
     }
 
     public static void main(String[] args) throws FileNotFoundException {

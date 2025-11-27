2941e7948788771fbfb15f0a6b205670267bdf6c
BowenNB <2923591580@qq.com>
2025-11-27 16:37:34 +0800

    refactor: 移除EasyExcel的excelType配置并优化csv生成逻辑

--- yubi_backend/src/test/java/com/yupi/springbootinit/utils/EasyExcelTest.java
+++ yubi_backend/src/test/java/com/yupi/springbootinit/utils/EasyExcelTest.java
@@ -5,6 +5,7 @@ import com.alibaba.excel.support.ExcelTypeEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;

a6fde90d41fa2c9d1bf6bfecd89bb563f5b47dc0
BowenNB <2923591580@qq.com>
2025-12-05 22:04:29 +0800

    feat(controller): 添加文件大小和后缀校验 feat(mapper): 新增查询图表数据的方法 test(mapper): 添加ChartMapper测试用例 style(frontend): 增加全局样式类margin-16 refactor(frontend): 优化MyChart页面结构和功能

--- yubi_backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
+++ yubi_backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -1,7 +1,7 @@
 package com.yupi.springbootinit.mapper;
 
-import com.yupi.springbootinit.model.entity.Chart;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.springbootinit.model.entity.Chart;
 import java.util.List;
 import java.util.Map;
 
@@ -18,8 +18,3 @@ public interface ChartMapper extends BaseMapper<Chart> {
      */
     List<Map<String, Object>> queryChartData(String querySql);
 }
-
-
-
-
-
--- yubi_backend/src/test/java/com/yupi/springbootinit/mapper/ChartMapperTest.java
+++ yubi_backend/src/test/java/com/yupi/springbootinit/mapper/ChartMapperTest.java
@@ -15,10 +15,11 @@ class ChartMapperTest {
 
     @Resource
     private ChartMapper chartMapper;
+
     @Test
     void queryChartData() {
         String chartId = "1993964361461948418";
-        String querySql = String.format("select * from chart where id = '%s'", chartId);
+        String querySql = String.format("select * from chart_%s", chartId);
         List<Map<String, Object>> resultData = chartMapper.queryChartData(querySql);
         System.out.println(resultData);
     }

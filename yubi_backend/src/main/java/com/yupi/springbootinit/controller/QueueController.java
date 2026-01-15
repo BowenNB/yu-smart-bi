fd5343f674f9ff2124bd2f13baffb584aecfad34
BowenNB <2923591580@qq.com>
2026-01-15 13:28:58 +0800

    2026.01.15,添加线程池实现异步，目前在写Controller

---
+++ yubi_backend/src/main/java/com/yupi/springbootinit/controller/QueueController.java
@@ -0,0 +1,61 @@
+package com.yupi.springbootinit.controller;
+
+import cn.hutool.core.io.FileUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.gson.Gson;
+import com.sun.corba.se.spi.orbutil.threadpool.ThreadPoolManager;
+import com.yupi.springbootinit.annotation.AuthCheck;
+import com.yupi.springbootinit.common.BaseResponse;
+import com.yupi.springbootinit.common.DeleteRequest;
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.common.ResultUtils;
+import com.yupi.springbootinit.constant.CommonConstant;
+import com.yupi.springbootinit.constant.UserConstant;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.exception.ThrowUtils;
+import com.yupi.springbootinit.manager.AiManager;
+import com.yupi.springbootinit.manager.RedisLimiterManager;
+import com.yupi.springbootinit.model.dto.chart.*;
+import com.yupi.springbootinit.model.entity.Chart;
+import com.yupi.springbootinit.model.entity.User;
+import com.yupi.springbootinit.model.vo.BiResponseVO;
+import com.yupi.springbootinit.service.ChartService;
+import com.yupi.springbootinit.service.UserService;
+import com.yupi.springbootinit.utils.ExcelUtils;
+import com.yupi.springbootinit.utils.SqlUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+@RestController
+@RequestMapping("/queue")
+@Slf4j
+public class QueueController {
+    @Resource
+    // 自动注入一个线程池的实例
+    private ThreadPoolExecutor threadPoolExecutor;
+
+    @GetMapping("/add")
+    // 接收一个参数name，然后将任务添加到线程池中
+    public void add(String name){
+        // 使用CompletableFuture运行一个异步任务
+        CompletableFuture.runAsync(() -> {
+
+        })
+
+    }
+
+}
